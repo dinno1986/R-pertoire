07d744bf65ac701ec753e428d57650f1bbdc7158
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-31 11:23:44 +0200

    supprimer une ligne du tableau

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ $("#enregistrer").click(function(){
 	var tNom = $(selecteurJquery).val();
 	var tPrenom = $("#prénom").val();
 	var tAge = $("#age").val();
+	
 
 	var personne={"nom":tNom,"prénom":tPrenom,"age":tAge};
 	repertoire.push(personne);
 	console.log(repertoire);
-	$(".table").append($("<tr><td>"+tNom +"</td><td>"+tPrenom +"</td><td>"+tAge +"</td></tr>"));
+	$(".table").append($("<tr><td>"+tNom +"</td><td>"+tPrenom +"</td><td>"+tAge +"</td><td>"+"<button class='deleteButton'>X</button>"+"</td></tr>"));
+
+	//$(".efface").click(function(){$(".table").remove(
+
+	});
+
+$("tbody").delegate(".deleteButton","click", function(){
+	$(this).parent().parent().remove();
 });
 
 //$("input").val(""); efface l'input de ces donnés
